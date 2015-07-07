63db08c7cf3c166d354fb0481f5fae13fab4b021
Vinicius <vinicius.remigio@gmail.com>
2015-07-07 09:21:15 -0300

    montando workspace em /root/workspace

--- init_rstudio.sh
+++ init_rstudio.sh
@@ -2,7 +2,7 @@
 
 echo Inicializando RStudio Server...
 
-docker run -dp 8787:8787  --name=rstudio --hostname=rserver -v /root:/home/rstudio/ -v /root/lib:/usr/local/lib/R/site-library/ -e ROOT=TRUE rocker/rstudio
+docker run -dp 8787:8787  --name=rstudio --hostname=rserver -v /root/workspace:/home/rstudio/ -v /root/lib:/usr/local/lib/R/site-library/ -e ROOT=TRUE rocker/rstudio
 
 echo RStudio bound na porta 8787
 
